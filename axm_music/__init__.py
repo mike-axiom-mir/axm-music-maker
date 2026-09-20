a82fe1cbc3fe3fe7d774918ef91b21272ac364c9
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 07:27:09 +0200

    Export music render plan adapter

--- axm_music/__init__.py
+++ axm_music/__init__.py
@@ -2,9 +2,12 @@
 
 from .adaptive import resolve_transition
 from .project import ProjectValidationError, canonical_json, project_hash, validate_project
+from .render_plan import RENDER_PLAN_SCHEMA, build_render_plan
 
 __all__ = [
     "ProjectValidationError",
+    "RENDER_PLAN_SCHEMA",
+    "build_render_plan",
     "canonical_json",
     "project_hash",
     "resolve_transition",
